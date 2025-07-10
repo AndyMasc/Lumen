6f3dd58ee3976bcfff2a60d94d3db57276402801
AndyMasc <andy.mascarenhas12@gmail.com>
2025-07-10 13:26:42 +1200

    Improved homepage and added register page

--- authenticate/views.py
+++ authenticate/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+
+def register(request):
+    form = CreateUserForm()
+    context = {'form':form}
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    return render(request, 'authenticate/register.html', context)
+
+def login(request):
+    return HttpResponse('Login')
