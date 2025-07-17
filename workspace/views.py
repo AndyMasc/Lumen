319a01b0b4bb3b5b1c9ef667e6fe97a929a245a7
AndyMasc <andy.mascarenhas12@gmail.com>
2025-07-17 20:56:39 +1200

    Seperate different code logic into functions

--- workspace/views.py
+++ workspace/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import StartupIdea, StartupEvaluation
@@ -22,14 +22,20 @@ def add_startup(request):
         return redirect("workspace:dashboard")
     return render(request, 'workspace/add_startup.html')
 
+@login_required
 def startup_evaluation(request, StartupIdea_id):
     startup = StartupIdea.objects.get(user=request.user, id=StartupIdea_id)
 
+    evaluation = create_evaluation(startup)
+
+    return render(request, 'workspace/startup_evaluation.html', {'startup':startup, 'evaluation':evaluation})
+
+def create_evaluation(startup):
     client = genai.Client(api_key=settings.GEMINI_API_KEY)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=f"Critically evaluate the following startup idea: {startup.description}. Format your response as plain text (no markdown, no code blocks). Provide a detailed analysis including strengths, weaknesses, and potential market impact, viability and all other factors. Keep answer informative, but concise. 2 paragraphs maximum."
+        contents=f"Critically evaluate the following startup idea: {startup.description}. Format your response as plain text (no markdown, no code blocks). Provide a detailed analysis including strengths, weaknesses, and potential market impact, viability and all other factors. Keep answer informative, but concise. 1 paragraph maximum."
     )
     evaluation = StartupEvaluation(startup=startup, evaluation_text=response.text)
     evaluation.save()
-    return render(request, 'workspace/startup_evaluation.html', {'startup':startup, 'evaluation':evaluation})
+    return evaluation
