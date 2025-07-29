ad84d8c57fa3a9192c424bfbbbd8d03d395e32ce
AndyMasc <andy.mascarenhas12@gmail.com>
2025-07-29 20:49:36 +1200

    Add koyeb to allowed hosts

--- Lumen/settings.py
+++ Lumen/settings.py
@@ -48,7 +48,7 @@ SECURE_HSTS_PRELOAD = True  # Preload HSTS for all subdomains
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.koyeb.app', '127.0.0.1']
 
 # Application definition
 
